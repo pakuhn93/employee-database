9d43900663e77586c93f0afeb0c7369db0ee6453
pakuhn93 <pakuhn93@gmail.com>
2023-06-28 23:16:30 -0700

    trying to work on the role area, keep running into conflicts. maybe a new solution should be attempted.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const inquirer = require('inquirer');
 const db = require('./db/connection');
 
 const choicesMain = ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'];
+const [menuDepartment, menuRole, menuEmployee] = ['department', 'role', 'employee'];
 
+// navigates back to the main menu if the user confirms
+// if the user rejects, exits the program
 const navHome = async () => {
     await inquirer
         .prompt([
@@ -26,27 +29,85 @@ const navHome = async () => {
         });
 }
 
+// adds data to an existing table
+// parameter takes in a string specifying which table to use
 const addData = async (table) => {
-    await inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'department',
-                message: 'Department Name:'
-            }
-        ])
-        .then(( {department} ) =>{
-            db.query(`INSERT INTO department (name) VALUES ('${department}')`, (err, res) => {
-                console.log(`'${department}' has been added to the list of departments.`);
-                navHome();
+    switch(table){
+        case menuDepartment:
+            await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'department',
+                    message: 'Department to be added:'
+                }
+            ])
+            .then(( {department} ) =>{
+                db.query(`INSERT INTO department (name) VALUES ('${department}')`, (err, res) => {
+                    console.log(`'${department}' has been added to the list of departments.`);
+                    navHome();
+                });
+            })
+            .catch((err) => {
+                if(err){
+                    console.log('An error has occurred. Please try again.');
+                }
             });
-        })
-        .catch((err) => {
-            if(err){
-                console.log('An error has occurred. Please try again.');
-            }
-        });
-    
+            break;
+
+        case menuRole:
+            await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'role',
+                    message: 'Role to be added:'
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: "Enter the role's salary:",
+                    validate: function validateNumber(input){
+                        if(isNaN(input)){
+                            return `\n${input} is not a number value. Please enter a number value.`;
+                        } else { return true; }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'department',
+                    message: 'Enter the department of the new role:',
+                    validate: function validateDepartment(input){
+                        const myQuery = `EXISTS(SELECT 1 FROM department WHERE name = '${input}')`;
+                        console.log(`\n${myQuery}`);
+                        db.query(`SELECT ${myQuery}`, (err, res) => {
+                            console.log(`\n${JSON.stringify(res)}`);
+                            if(err){console.log(err);}
+                            if(res == 0){
+                                return `\n'${input}' is not a valid department.`;
+                            } else { 
+                                return true;  
+                            }
+                        });
+                    }
+                }
+            ])
+            .then(( {role, salary, department} ) => {
+                db.query(`INSERT INTO role (title, salary, department_id) VALUES (${role}, ${salary}, 100)`, (err, res) => {
+                    console.log(`${role} successfully added to the list of roles`);
+                });
+            })
+            .catch((err) => {
+                if(err){
+                    console.log('An error has occurred. Please try again.');
+                }
+            })
+            break;
+        case menuEmployee:
+            break;
+        default:
+            break;
+    }
 }
 
 // initialize the program
@@ -62,9 +123,9 @@ async function init(){
             }
         ])
         // based on selection from above prompt, decide what to do
-        // CONSIDER changing switch to for loop with if conditional inside
         .then(( {selection} ) => {
             switch (selection){
+                // View Departments
                 case choicesMain[0]:
                     console.log('Viewing all departments...');
                     db.query(`SELECT name AS Departments FROM department`, (err, res) => {
@@ -76,7 +137,7 @@ async function init(){
                         }
                     });
                     break;
-
+                // View Roles
                 case choicesMain[1]:
                     console.log('Viewing all roles...');
                     db.query(`SELECT title AS Roles FROM role`, (err, res) => {
@@ -88,7 +149,7 @@ async function init(){
                         }
                     });
                     break;
-
+                // View Employees
                 case choicesMain[2]:
                     console.log('Viewing all employees...');
                     db.query(`SELECT first_name AS First_Name, last_name AS Last_Name FROM employee`, (err, res) => {
@@ -100,26 +161,29 @@ async function init(){
                         }
                     });
                     break;
-
+                // Add Department
                 case choicesMain[3]:
                     console.log('Adding department...');
-                    addData();
+                    addData(menuDepartment);
                     break;
-
+                // Add Role
                 case choicesMain[4]:
                     console.log('Adding role...');
+                    addData(menuRole);
                     break;
-
+                // Add Employee
                 case choicesMain[5]:
                     console.log('Adding employee...');
+                    addData(menuEmployee);
                     break;
-
+                // Update Employee
                 case choicesMain[6]:
                     console.log('Updating employee...');
                     break;
-
+                // error check
                 default:
                     console.log('An internal error has occurred... Please try again.');
+                    navHome();
                     break;
             }
         })
@@ -131,4 +195,5 @@ async function init(){
         });
 }
 
+// initializes the program
 init();
